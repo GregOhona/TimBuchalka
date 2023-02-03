c4d900513e5d127ba3e076bcda6bd502a6715315
GregOhona <greg@ohona.com>
2023-02-03 14:13:49 +1000

    Initial commit

---
+++ MasterChallenge.java
@@ -0,0 +1,27 @@
+public class MasterChallenge {
+    public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelComplete = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        if (gameOver){
+            finalScore += (levelComplete * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        score = 10_000;
+        levelComplete = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver){
+            finalScore += (levelComplete * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
+}
